27500dd7f76324835668ddc9c2e9aab5a384300a
mototasmafunqueiro <tiago.giroto.neto1@gmail.com>
2019-10-08 11:24:25 -0300

    teste

--- programa.js
+++ programa.js
@@ -1,17 +1,42 @@
-function soma () { 
+var operando1 = -1;
+var operando2 = -1;
+var operador = '';
 
+function digitaOperando (numero){
+    let visor = document.getElementById("visor");
+
+    if(operador == ''){
+        operando1 = numero;
+        visor.innerHTML = operando1;
+    }else {
+        operando2 = numero;
+        visor.innerHTMl = operando1 + ' ' + operador + ' ' +operando2;
+    }
+   
 }
-function divisao () { 
 
-}
-function subtracao () { 
-
-}
-function multiplicacao () { 
-
-}
-function resultado () { 
-
+function digitaOperador(simbolo){
+    let visor = document.getElementById("visor");
+    operador = simbolo;
+    visor.innerHTML = operando1 + ' ' + operador;
 }
 
+function executa(){
+    let resultado =0;
+    let visor = document.getElementById("visor");
+    switch(operador){
+        case '+': resultado = operando1 + operando2; break;
+        case '-': resultado = operando1 - operando2; break;
+        case '/': resultado = operando1 / operando2; break;
+        case 'x': resultado = operando1 * operando2; break;
+    }
 
+    visor.innerHTML = operando1 + ' ' + operador + ' ' + operando2 + ' = ' + resultado;
+}
+function limpa(){
+    let visor = document.getElementById("visor");
+    operando1 = -1;
+    operando2 = -1;
+    operador ='';
+    visor.innerhtml ='';
+}
